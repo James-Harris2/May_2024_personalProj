46abba250fdd4c2850cf8cc41a798d401eacbeb4
James Harris <jameseharrisii@gmail.com>
2024-05-15 15:51:09 -0700

    init commit; Working with Materials UI_building better functionality

---
+++ May_2024_personalProj/stylingComponents/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import { Autocomplete, TextField } from '@mui/material';
+
+import './App.css'
+
+function App() {
+  const options = ['Apple', 'Honey', 'Tea', 'Pears', 'Oranges']
+  return (
+    <div className='App'>
+      <h1>Welcome Shopper James</h1>
+        <h6>of course we care! </h6>
+          <p> now pick a item </p>
+        <Autocomplete
+          options={options}
+          renderInput={(params) => <TextField {... params} label='Items' />} />
+        
+    </div>
+  )
+}
+
+export default App
