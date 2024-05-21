3b9e610e6dc242c12f522009702b7139422e7b8d
James Harris <jameseharrisii@gmail.com>
2024-05-21 09:58:15 -0700

    Making changes to file structure to keep DRY

---
+++ May_2024_personalProj/softwareDeveloper_portfolio_2024/Portfolio2/src/Footer.jsx
@@ -0,0 +1,18 @@
+import { Typography } from "@mui/material";
+import React from 'react'
+
+{ Typography}
+
+
+
+function Footer() {
+  return (
+    <div>
+        <Typography variant="subtitle1" >
+            James E Harris II
+        </Typography>
+    </div>
+  )
+}
+
+export default Footer
