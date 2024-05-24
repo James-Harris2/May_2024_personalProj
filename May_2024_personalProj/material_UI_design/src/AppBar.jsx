1fb48935bc2bb7c4e57c4a86f5ea9665f7976809
James Harris <jameseharrisii@gmail.com>
2024-05-24 13:05:47 -0700

    Different layout experimentation

--- May_2024_personalProj/material_UI_design/src/AppBar.jsx
+++ May_2024_personalProj/material_UI_design/src/AppBar.jsx
@@ -1,10 +1,15 @@
-import {AppBar} from '@mui/material';
+import {AppBar, Button, Typography} from '@mui/material';
+
+// Experimenting with Different layout
 
 
 function Head() {
   return (
     <div>
-        <AppBar>
+        <AppBar elevation="8">
+            <Typography> J.E.H.II</Typography>
+            <Button color="error">Login</Button>
+            
         Build Better User UI Components using Material UI
 
         </AppBar>
